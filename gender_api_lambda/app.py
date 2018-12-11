3a380bde670651dae7b1efb0929a609868a8f8c2
miguel <mglfarrajota@outlook.com>
2018-12-11 09:40:29 +0000

    Update gender api lambda

--- gender_api_lambda/app.py
+++ gender_api_lambda/app.py
@@ -10,7 +10,7 @@ table = dynamodb.Table(dynamodb_table_name)
 def lambda_handler(event, context):
     """Retrieve gender information of a client id."""
     client_id = event["pathParameters"]["clientid"]
-    response = table.get_item(Key={"clientid": client_id})
+    response = get_item_dynamodb(client_id)
     if "Item" in response:
         data = response["Item"]
         output = {"clientid": client_id, "gender": data["gender"]}
@@ -20,3 +20,8 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps(output),
     }
+
+
+def get_item_dynamodb(client_id):
+    response = table.get_item(Key={"clientid": client_id})
+    return response
