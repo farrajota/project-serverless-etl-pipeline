7588a8f98db0a720422f1ea9980a89e216689457
miguel <mglfarrajota@outlook.com>
2018-12-11 00:23:52 +0000

    Add API Gateway service

---
+++ gender_api_lambda/app.py
@@ -0,0 +1,22 @@
+import os
+import boto3
+import json
+
+dynamodb_table_name = os.environ["DYNAMODB_TABLE"]
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(dynamodb_table_name)
+
+
+def lambda_handler(event, context):
+    """Retrieve gender information of a client id."""
+    client_id = event["pathParameters"]["clientid"]
+    response = table.get_item(Key={"clientid": client_id})
+    if "Item" in response:
+        data = response["Item"]
+        output = {"clientid": client_id, "gender": data["gender"]}
+    else:
+        output = {"clientid": client_id, "gender": "Null"}
+    return {
+        "statusCode": 200,
+        "body": json.dumps(output),
+    }
