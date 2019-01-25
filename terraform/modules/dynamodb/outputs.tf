9011df82fb290471c86cec88dbdb5a5ce4463e9e
miguel <mglfarrajota@outlook.com>
2019-01-25 15:09:11 +0000

    Rename outputs in dynamodb module

--- terraform/modules/dynamodb/outputs.tf
+++ terraform/modules/dynamodb/outputs.tf
@@ -1,7 +1,7 @@
-output "dynamodb_arn" {
+output "dynamodb_table_arn" {
   value = "${aws_dynamodb_table.gender_db.arn}"
 }
 
 output "dynamodb_table_name" {
-  value = "${var.table_name}"
+  value = "${aws_dynamodb_table.gender_db.id}"
 }
