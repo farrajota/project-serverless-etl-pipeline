00e412130682f7323f0e3fc81f4612d1f83dee18
miguel <mglfarrajota@outlook.com>
2019-02-04 12:02:03 +0000

    Move kinesis firehose lambda roles + policies to iam.tf

--- terraform/modules/kinesis_firehose/iam.tf
+++ terraform/modules/kinesis_firehose/iam.tf
@@ -1,3 +1,7 @@
+####################
+# Kinesis Firehose
+####################
+
 resource "aws_iam_role" "kinesis_firehose" {
   name = "${var.project_name}-${var.firehose_name}-role"
 
@@ -59,6 +63,54 @@ resource "aws_iam_role_policy" "kinesis_firehose_s3" {
             "Resource": [
               "${var.s3_bucket_arn}",
               "${var.s3_bucket_arn}*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+
+####################
+# Lambda
+####################
+
+resource "aws_iam_role" "lambda_iam_role" {
+  name = "${var.project_name}-${var.firehose_name}-lambda_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "lambda_policy_logs" {
+  name = "${var.project_name}-${var.firehose_name}-lambda_logs"
+  role = "${aws_iam_role.lambda_iam_role.id}"
+
+  policy = <<EOF
+{
+    "Statement": [
+        {
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "*"
             ]
         }
     ]
